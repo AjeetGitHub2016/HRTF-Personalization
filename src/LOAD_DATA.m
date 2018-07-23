b7032f746e49c0ae930cff836d836503ae9b7bb4
AjeetGitHub2016 <er.ajeetsinghyadav@gmail.com>
2018-07-24 00:10:42 +0530

    Added Load_data file

---
+++ src/LOAD_DATA.m
@@ -0,0 +1,14 @@
+disp('Loading processed data');
+load('preprocessed data\ANTHRO_data.mat');
+load('preprocessed data\ITD_data.mat');
+load('preprocessed data\HRIR_data.mat');
+load('preprocessed data\HRTF_data.mat');
+load('preprocessed data\mean_vector.mat');
+load('preprocessed data\directional_mean_vector.mat');
+load('preprocessed data\weight_vectors.mat');
+load('preprocessed data\PC_mtx.mat');
+load('preprocessed data\ITD_data.mat');
+disp('Loading processed data Done');
+
+
+
